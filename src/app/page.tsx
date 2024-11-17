7527c12b997824c45ddb71dd5faf60da99f64b69
bisenteherradura <vincentaisner12@gmail.com>
2024-11-17 22:42:45 +0800

    hello

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { signOut } from "./(auth)/actions";
 import { Button } from "@/components/ui/button";
 import { joinOrg } from "@/actions/org-actions";
 import Link from "next/link";
-
+//hi
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
