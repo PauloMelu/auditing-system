913d46e5e51f97783e0ffa411733eb9cc9b1e845
bisenteherradura <vincentaisner12@gmail.com>
2024-11-18 15:18:14 +0800

    homepage fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,18 +8,23 @@ import './style.css'
 //hi
 export default function Home() {
   return (
-    <nav className="nav">
-      <div className="logo">
-        <h1>BudgeThink</h1>
-      </div>
-      <div className="Login">
-        <a href="/login">
-          <button className="btn loginbtn" id="login">Login</button> &nbsp;
-        </a>
-        <a href="/signup">
-          <button className="btn" id="signup">Signup</button>
-        </a>
-      </div>
-    </nav>
+    <body>
+      <nav className="nav">
+
+        <div className="logo">
+          <h1>BudgeThink</h1>
+        </div>
+        
+        <div className="Login">
+          <a href="/login">
+            <button className="btn loginbtn" id="login">Login</button> &nbsp;
+          </a>
+          <a href="/signup">
+            <button className="btn" id="signup">Signup</button>
+          </a>
+        </div>
+      </nav>
+    </body>
+
   );
 }
