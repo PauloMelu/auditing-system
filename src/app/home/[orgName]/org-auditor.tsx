a00c6b3617326f78a15536fdf9733c53b3a5f295
Melu <s2022100348@firstasia.edu.ph>
2024-12-10 23:20:19 +0800

    org auditor view

--- src/app/home/[orgName]/org-auditor.tsx
+++ src/app/home/[orgName]/org-auditor.tsx
@@ -8,6 +8,7 @@ import { Button } from "@/components/ui/button";
 import { OrganizationMembersTbl } from "@/types/types";
 import { redirect } from "next/dist/server/api-utils";
 import Link from "next/link";
+import "./auditor-style.css"
 
 type Props = { orgMember: OrganizationMembersTbl }
 
@@ -22,61 +23,82 @@ export default async function OrgAuditor({ ...props }: Props) {
 
   return (
     <div>
-      {orgMember.orgName} - {orgMember.userType}
-      <br />
-      Money: {orgMember.money}
-      <br />
-      <Button variant="link">
-        <Link href={`./${orgMember.orgName}/members`}>
-          Members List
-        </Link>
-      </Button>
-      <br />
-      <br />
+      <header className="dashboard">
+        <h1>Dashboard </h1>
+        <h1>User: {orgMember.userType}</h1>
+      </header>
 
-      <form>
-        <input name="orgName" type="hidden" value={orgMember.orgName} />
 
-        <input name="eventName" type="text" placeholder="Event Name" required />
-        <input name="budget" type="number" placeholder="Budget" required />
-        <Button variant="outline" formAction={createEvent}>Create Event</Button>
-      </form>
-
-      <br />
-      <br />
-
-      {events.map(event => (
-
-        <div key={event.eventName}>
-          <form>
+      <div className="sidebar">
+        <h1>
+          Money: {orgMember.money}php
+          <a href={`./${orgMember.orgName}/members`}>
             <Button variant="link">
-              <Link href={`./${orgMember.orgName}/${event.eventName}`} >
-                {event.eventName}
-              </Link>
+              Members List
             </Button>
-            - {event.budget}
-
-            <input name="eventName" type="hidden" value={event.eventName} />
-            <input name="userId" type="hidden" value={orgMember.userId} />
+          </a>
+        </h1>
+        <center>
+          <form>
             <input name="orgName" type="hidden" value={orgMember.orgName} />
-            <input name="budget" type="hidden" value={event.budget} />
-            {(event.budgetTaken) ? (
-              <Button variant="outline" disabled>
-                Budget Claimed
-              </Button>
-            ) : (
-              <Button variant="outline" formAction={claimBudget}>
-                Claim Budget
-              </Button>
-            )
-            }
 
-
+            <input name="eventName" type="text" placeholder="Event Name" required />
+            <input name="budget" type="number" placeholder="Budget" required />
+            <Button variant="outline" formAction={createEvent}>Create Event</Button>
           </form>
+        </center>
+      </div>
 
+      <div className="events-wrapper">
+        <h1>Events:</h1>
+        <div id="events">
+          <div>Event Name:</div>
+          <div>Budget:</div>
+          <div></div>
         </div>
 
-      ))}
+        {events.map(event => (
+
+          <div key={event.eventName}>
+
+            <form>
+              <div id="events">
+                <div>
+                  <Button variant="link">
+                    <Link href={`./${orgMember.orgName}/${event.eventName}`} >
+                      <span>{event.eventName}</span>
+                    </Link>
+                  </Button>
+                </div>
+                <div>
+                  {event.budget} php
+                </div>
+
+                <input name="eventName" type="hidden" value={event.eventName} />
+                <input name="userId" type="hidden" value={orgMember.userId} />
+                <input name="orgName" type="hidden" value={orgMember.orgName} />
+                <input name="budget" type="hidden" value={event.budget} />
+                <div>
+                  {(event.budgetTaken) ? (
+                    <Button variant="outline" disabled>
+                      Budget Claimed
+                    </Button>
+                  ) : (
+                    <Button variant="outline" formAction={claimBudget}>
+                      Claim Budget
+                    </Button>
+                  )
+                  }
+                </div>
+              </div>
+            </form>
+
+
+          </div>
+
+        ))}
+
+      </div>
     </div>
   )
 };
